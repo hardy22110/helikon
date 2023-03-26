0c519112fdf3e79439f2a1b22c85d916158d52d4
hardy22110 <hardy22110@gmail.com>
2023-03-26 19:11:54 +0800

    Feat: delay function

---
+++ src/utils/promiseUtils/delay.ts
@@ -0,0 +1,28 @@
+import setNumberValue from '@/utils/numberUtils/setNumberValue'
+/**
+ * Delays the execution of the function by the given amount of time.
+ * If the given time is less than zero, the function resolves after 0 seconds.
+ * If a non-number value is passed in, treat it as 0
+ * @since vx.x.x
+ * @category Promise
+ * @param time - The amount of time to delay the execution, in milliseconds.
+ * @returns A promise that resolves after the specified delay.
+ *
+ * @example
+ * delay(1000).then(() => {
+ *    console.log('one second later')
+ * })
+ *
+ * @example
+ * async () => {
+ *    await delay(1000)
+ *    console.log('one second later')
+ * }
+ *
+ */
+export default function delay(time?: number) {
+  return new Promise<void>((resolve) => {
+    const ms = setNumberValue(time, 0)
+    setTimeout(() => resolve(), ms < 0 ? 0 : ms)
+  })
+}
